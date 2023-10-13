b45b8bb87847dabd22d3e8aea66f48930910fb02
Alexander Kalinin <alex@kalinin.uk>
2023-10-13 21:12:55 +0100

    improve

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,49 @@
-use std::fs;
 use aoc_2023::solution::Solution;
 use aoc_2023::solutions;
+use chrono::prelude::*;
 use clap::Parser;
+use std::fs;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Which day to run solution for
-    #[arg(short, long, default_value_t = 1)]
-    day: u8,
+    /// Which day to run solution for. Defaults to current day of the month
+    #[arg(short, long)]
+    day: Option<u32>,
 
     /// Filename of input
     #[arg(short, long)]
     file: String,
 }
 
+fn get_day(input: Option<u32>) -> u32 {
+    match input {
+        Some(day) => day,
+        None => {
+            let local: DateTime<Local> = Local::now();
+            local.day()
+        }
+    }
+}
 
 fn main() {
     let Args { day, file } = Args::parse();
 
-    let solutions: Vec<Box<dyn Solution>> = vec![
-        Box::new(solutions::Day1),
-    ];
+    let solutions: Vec<Box<dyn Solution>> = vec![Box::new(solutions::Day1)];
+
+    let day = get_day(day);
 
     println!("Getting solution for day {day} with file {file}",);
 
     let file_contents = fs::read_to_string(&file).expect("Unable to read file");
-    let solution = solutions.get(day as usize - 1).unwrap();
+    let solution = solutions.get(day as usize - 1).expect(
+        format!(
+            "No solution for day {day}. Only have days 1-{}",
+            solutions.len()
+        )
+        .as_str(),
+    );
     solution.part1(&file_contents);
     solution.part2(&file_contents);
 }
