0ca02d6775619a65cd50ed8687e21114db112603
Alexander Kalinin <alex@kalinin.uk>
2023-12-10 13:26:25 +0700

    day 7 part 2

--- src/solutions/day_7.rs
+++ src/solutions/day_7.rs
@@ -1,28 +1,28 @@
-use crate::util::string_to_numbers;
 use sscanf::scanf;
-use std::str::FromStr;
 
 pub struct Day7;
 impl crate::Solution for Day7 {
     fn part1(&self, input: &str) {
-        part1(input);
+        println!("{}", part1(input));
     }
     fn part2(&self, input: &str) {
-        part2(input);
+        println!("{}", part2(input));
     }
 }
 
-fn part1(input: &str) {
+fn part1(input: &str) -> usize {
+    solve(input)
+}
+
+fn part2(input: &str) -> usize {
+    solve(&input.replace("J", "*"))
+}
+
+fn solve(input: &str) -> usize {
     let mut entries = input
         .lines()
         .map(|l| scanf!(l, "{}", Entry).unwrap())
         .collect::<Vec<Entry>>();
-    println!("{:?}", entries);
-    // for entry in entries {
-    //     let hand = entry.hand.chars().collect::<Vec<char>>();
-    //     let hand_type = hand_type(hand);
-    //     println!("{} is {:?}", entry.hand, hand_type);
-    // }
 
     entries.sort_by(|a, b| {
         let a_hand = a.hand.chars().collect::<Vec<char>>();
@@ -31,8 +31,14 @@ fn part1(input: &str) {
         let b_hand_type = hand_type(b_hand.clone());
         if a_hand_type == b_hand_type {
             cmp_hand_values(
-                a_hand.iter().map(|c| card_char_to_value(*c)).collect::<Vec<usize>>(),
-                b_hand.iter().map(|c| card_char_to_value(*c)).collect::<Vec<usize>>(),
+                a_hand
+                    .iter()
+                    .map(|c| card_char_to_value(*c))
+                    .collect::<Vec<usize>>(),
+                b_hand
+                    .iter()
+                    .map(|c| card_char_to_value(*c))
+                    .collect::<Vec<usize>>(),
             )
         } else {
             let a_hand_type_value = hand_type_to_value(a_hand_type);
@@ -43,14 +49,10 @@ fn part1(input: &str) {
     let mut total: usize = 0;
     for (i, entry) in entries.iter().enumerate() {
         let winnings = entry.bid * (i + 1);
-        let hand_type = hand_type(entry.hand.chars().collect::<Vec<char>>());
-        println!("{:?} {:?} rank {} wins {}", entry, hand_type, i + 1, winnings);
         total += winnings;
     }
-    println!("{}", total);
+    return total;
 }
-
-fn part2(input: &str) {}
 
 #[derive(sscanf::FromSscanf, Debug)]
 #[sscanf(format = "{hand} {bid}")]
@@ -66,6 +68,7 @@ fn card_char_to_value(c: char) -> usize {
         'Q' => 12,
         'J' => 11,
         'T' => 10,
+        '*' => 0,
         i => i.to_digit(10).unwrap() as usize,
     }
 }
@@ -99,21 +102,38 @@ fn hand_type(hand: Vec<char>) -> HandType {
         for card in hand {
             counts[card_char_to_value(card)] += 1;
         }
+        let jokers = counts[0];
+        counts.remove(0);
         let mut counts = counts.iter().filter(|c| **c > 0).collect::<Vec<&usize>>();
+        if counts.is_empty() {
+            if jokers == 5 {
+                return HandType::FiveOfAKind;
+            } else if jokers == 4 {
+                return HandType::FourOfAKind;
+            } else if jokers == 3 {
+                return HandType::ThreeOfAKind;
+            } else if jokers == 2 {
+                return HandType::Pair;
+            } else {
+                return HandType::HighCard;
+            }
+        }
         counts.sort();
         counts.reverse();
         let counts = counts.iter().map(|c| **c).collect::<Vec<usize>>();
-        if counts[0] == 5 {
+        if counts[0] + jokers == 5 {
             HandType::FiveOfAKind
-        } else if counts[0] == 4 {
+        } else if counts[0] + jokers == 4 {
             HandType::FourOfAKind
-        } else if counts[0] == 3 && counts[1] == 2 {
+        } else if (counts[0] == 3 && counts[1] == 2)
+            || (counts[0] == 2 && counts[1] == 2 && jokers == 1)
+        {
             HandType::FullHouse
-        } else if counts[0] == 3 {
+        } else if counts[0] + jokers == 3 {
             HandType::ThreeOfAKind
         } else if counts[0] == 2 && counts[1] == 2 {
             HandType::TwoPair
-        } else if counts[0] == 2 {
+        } else if counts[0] + jokers == 2 {
             HandType::Pair
         } else {
             HandType::HighCard
@@ -133,3 +153,34 @@ fn cmp_hand_values(a: Vec<usize>, b: Vec<usize>) -> std::cmp::Ordering {
     }
     std::cmp::Ordering::Equal
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1() {
+        let input = r#"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"#;
+        assert_eq!(part1(input), 6440);
+    }
+
+    #[test]
+    fn p2() {
+        let input = r#"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"#;
+        assert_eq!(part2(input), 5905);
+    }
+
+    #[test]
+    fn p2_hand_types() {
+        assert_eq!(hand_type("*22TT".chars().collect::<Vec<char>>()), HandType::FullHouse);
+    }
+}
+
